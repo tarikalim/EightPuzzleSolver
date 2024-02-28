5d15824f61534dd9b3959a888f265153fcbe158e
tarikalim <tarikalim1899@gmail.com>
2024-02-28 11:12:05 +0300

    move control is directly controlled by AStar class, so move functions in Board class are simplified.

--- src/Board.java
+++ src/Board.java
@@ -75,61 +75,38 @@ public class Board {
         }
     }
 
-    // A method for moving the empty cell right
-    public boolean moveRight() {
-        // the empty cell cannot go right if it is already at the rightmost column
-        if (emptyCellCol == 2)
-            return false; // return false as the empty cell cannot be moved
+    public void moveRight() {
         // replace the empty cell with the tile on its right
         tiles[emptyCellRow][emptyCellCol] = tiles[emptyCellRow][emptyCellCol + 1];
         tiles[emptyCellRow][emptyCellCol + 1] = null;
         // update the column index of the empty cell
         emptyCellCol++;
-        // return true as the empty cell is moved successfully
-        return true;
     }
 
-    // A method for moving the empty cell left
-    public boolean moveLeft() {
-        // the empty cell cannot go left if it is already at the leftmost column
-        if (emptyCellCol == 0)
-            return false; // return false as the empty cell cannot be moved
+    public void moveLeft() {
         // replace the empty cell with the tile on its left
         tiles[emptyCellRow][emptyCellCol] = tiles[emptyCellRow][emptyCellCol - 1];
         tiles[emptyCellRow][emptyCellCol - 1] = null;
         // update the column index of the empty cell
         emptyCellCol--;
-        // return true as the empty cell is moved successfully
-        return true;
     }
 
-    // A method for moving the empty cell up
-    public boolean moveUp() {
-        // the empty cell cannot go up if it is already at the topmost row
-        if (emptyCellRow == 0)
-            return false; // return false as the empty cell cannot be moved
+    public void moveUp() {
         // replace the empty cell with the tile above it
         tiles[emptyCellRow][emptyCellCol] = tiles[emptyCellRow - 1][emptyCellCol];
         tiles[emptyCellRow - 1][emptyCellCol] = null;
         // update the row index of the empty cell
         emptyCellRow--;
-        // return true as the empty cell is moved successfully
-        return true;
     }
 
-    // A method for moving the empty cell down
-    public boolean moveDown() {
-        // the empty cell cannot go down if it is already at the bottommost row
-        if (emptyCellRow == 2)
-            return false; // return false as the empty cell cannot be moved
+    public void moveDown() {
         // replace the empty cell with the tile below it
         tiles[emptyCellRow][emptyCellCol] = tiles[emptyCellRow + 1][emptyCellCol];
         tiles[emptyCellRow + 1][emptyCellCol] = null;
         // update the row index of the empty cell
         emptyCellRow++;
-        // return true as the empty cell is moved successfully
-        return true;
     }
+
 
     // A method for drawing the board by using the StdDraw library
     public void draw() {
@@ -168,7 +145,7 @@ public class Board {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 if (tiles[i][j] == null) {
-                    currentState[i][j] = 0; // Boş tile için 0 değeri kullanılır.
+                    currentState[i][j] = 0;
                 } else {
                     currentState[i][j] = tiles[i][j].getNumber();
                 }
