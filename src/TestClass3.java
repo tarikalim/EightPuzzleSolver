68e66620a0b2f458543d22f4d4695ce01d1f94cb
tarikalim <tarikalim1899@gmail.com>
2024-03-11 20:30:05 +0300

    comment line for duration test with and without heuristic funtions.

--- src/TestClass3.java
+++ src/TestClass3.java
@@ -12,36 +12,36 @@ public class TestClass3 {
         // Test for the solve method with heuristic.
         Board testBoardWithManhattan = new Board(testTiles);
         Function<int[][], Integer> heuristic1 = HeuristicFunctions::manhattan;
-        State.heuristicFunction = heuristic1; // Heuristik fonksiyonu ayarla
+        State.heuristicFunction = heuristic1;
 
         long startTimeWithHeuristic = System.nanoTime();
         String solutionWithHeuristic = AStarSolver.solve(testBoardWithManhattan);
         long endTimeWithHeuristic = System.nanoTime();
 
-        long durationWithHeuristic = (endTimeWithHeuristic - startTimeWithHeuristic); // Nanosaniye olarak süre
-        System.out.println("Manhattan ile çözüm süresi: " + durationWithHeuristic + " nanosaniye. Çözüm yolu: " + solutionWithHeuristic);
+        long durationWithHeuristic = (endTimeWithHeuristic - startTimeWithHeuristic);
+        System.out.println("Duration with manhattan: " + durationWithHeuristic + " nanosecond. Solution path: " + solutionWithHeuristic);
 
         Board testBoardWithMisPlacedTiles = new Board(testTiles);
         Function<int[][], Integer> heuristic2 = HeuristicFunctions::misplacedTiles;
-        State.heuristicFunction = heuristic2; // Heuristik fonksiyonu ayarla
+        State.heuristicFunction = heuristic2;
 
         long startTimeWithMisPlacedTiles = System.nanoTime();
         String solutionWithMisPlacedTiles = AStarSolver.solve(testBoardWithMisPlacedTiles);
         long endTimeWithMisPlacedTiles = System.nanoTime();
 
         long durationWithMissPlacedTiles = (endTimeWithMisPlacedTiles - startTimeWithMisPlacedTiles); // Nanosaniye olarak süre
-        System.out.println("Miss Placed ile çözüm süresi: " + durationWithMissPlacedTiles + " nanosaniye. Çözüm yolu: " + solutionWithMisPlacedTiles);
+        System.out.println("Duration with Misplaced Tile: " + durationWithMissPlacedTiles + " nanosecond. Solution path: " + solutionWithMisPlacedTiles);
 
 
         // Test for the solve method without heuristic.
-        State.heuristicFunction = board -> 0; // Heuristiği etkisiz hale getir
+        State.heuristicFunction = board -> 0;
         Board testBoardWithoutHeuristic = new Board(testTiles);
 
         long startTimeWithoutHeuristic = System.nanoTime();
         String solutionWithoutHeuristic = AStarSolver.solve(testBoardWithoutHeuristic);
         long endTimeWithoutHeuristic = System.nanoTime();
 
-        long durationWithoutHeuristic = (endTimeWithoutHeuristic - startTimeWithoutHeuristic); // Nanosaniye olarak süre
-        System.out.println("Heuristik olmadan çözüm süresi: " + durationWithoutHeuristic + " nanosaniye. Çözüm yolu: " + solutionWithoutHeuristic);
+        long durationWithoutHeuristic = (endTimeWithoutHeuristic - startTimeWithoutHeuristic);
+        System.out.println("Duration without Heuristic: " + durationWithoutHeuristic + " nanosecond. Solution path: " + solutionWithoutHeuristic);
     }
 }
